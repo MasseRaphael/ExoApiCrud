7f8340751f8024d0502c6f3510809e49c2bc49e7
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-12-15 14:53:54 +0100

    intégration projet visual studios

---
+++ ApiMicromania/Controllers/MicromaniaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMicromania.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MicromaniaController : Controller
+    {
+        public JsonResult Index()
+        {
+            List<int> list = new List<int>
+            {
+                1,2,3,4,5
+            };
+
+            return (Json(list));
+        }
+    }
+}
